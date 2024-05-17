141f2c778675c871aa122302af7b6d742e1ba627
bt-png <brett.m.tharp@gmail.com>
2024-05-16 22:29:13 -0700

    a

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write('Hello')
+def run():
+    st.write('Hello')
+    with st.container():
+        val = st.file_uploader('Upload CSV', type='csv', accept_multiple_files=False)
+        if val is not None:
+            st.write(val)
+
+if __name__ == '__main__':
+    run()

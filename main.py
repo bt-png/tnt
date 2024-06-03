fb14591f737c1c786caccbb09fb050b5831125f7
bt-png <110639339+bt-png@users.noreply.github.com>
2024-06-02 19:43:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 import _process
+import _gitfiles
 import _pickle
 
 
@@ -11,8 +12,18 @@ def run():
     )
     st.write('Hello')
     with st.container():
-        val = st.file_uploader('Upload CSV', type={'csv'}, accept_multiple_files=False)
+        col1, col2 = st.columns([9,1])
+        val = col1.file_uploader('Upload CSV', type={'csv'}, accept_multiple_files=False)
         if val is not None:
+            col2.header('')
+            col2.header('')
+            if col2.button('Save'):
+                bitfile = val.getvalue()
+                _gitfiles.commit(
+                    filename='current_input.csv',
+                    message='api commit',
+                    content=bitfile
+                )
             st.markdown('---')
             df_tips, df_tips_adjusted = _process.run(val)
 

626d560bb57515654ca04b495feae1be44e43fde
bt-png <110639339+bt-png@users.noreply.github.com>
2024-08-08 17:25:00 -0700

    Update tipping2.py

--- pages/tipping2.py
+++ pages/tipping2.py
@@ -476,7 +476,8 @@ def TipsSum():
     df.loc[df.index[-1], 'Employee Name'] = 'Total'
     st.session_state['tipdata']['df_tipssum'] = df.copy()
     df.set_index(['Employee Name'], drop=True, inplace=True)
-    df = df.style.format('${:.2f}')
+    df = df.style.format('${:.2f}, subset=['Garden Tips', 'Regular Tips', 'Helper Tips'])
+    df = df.format('{:.2f}, subset=['Regular'])
     df = df.set_properties(subset = pd.IndexSlice[['Total'], :], **{'background-color' : 'lightgrey'})
     st.dataframe(df, hide_index=True,
                  column_order=['Employee Name', 'Regular', 'Garden Tips', 'Regular Tips', 'Helper Tips'],

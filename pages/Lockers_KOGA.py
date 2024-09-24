888f8540ef44179efca818fd9f3ad6c4064a2445
bt-png <110639339+bt-png@users.noreply.github.com>
2024-09-24 05:16:05 +0000

    sdf

--- pages/Lockers_KOGA.py
+++ pages/Lockers_KOGA.py
@@ -14,7 +14,10 @@ def show_combination(locker_num, _df):
 
 
 def next_combination(locker_num, _df):
-    _df.loc[_df[_df['Locker'] == locker_num].index, 'Current'] += 1
+    if _df.loc[_df[_df['Locker'] == locker_num].index, 'Current'] == 4:
+        _df.loc[_df[_df['Locker'] == locker_num].index, 'Current'] = 0
+    else:
+        _df.loc[_df[_df['Locker'] == locker_num].index, 'Current'] += 1
 
 
 def input_locker():
@@ -25,23 +28,30 @@ def load_df():
     df = pd.DataFrame({
         'Locker': [12, 13, 14, 15],
         'Current': [1, 2, 1, 3],
-        'Combinations': [[0,1,2,3,4], [0,1,2,3,4], [0,1,2,3,4], [0,1,2,3,4]],
-        'Assigned': ['', '', '', ''],
+        'Combinations': [['0-15-36','1-12-34','2-32-31','3-5-12','4-2-12'], [0,1,2,3,4], [0,1,2,3,4], [0,1,2,3,4]],
+        'Assigned': ['Tharp', '', '', ''],
         'Comments': ['', '', '', '']
     })
     return df
 
 
 if __name__ == '__main__':
-    df = load_df()
-    locker = input_locker()
+    if 'df' not in st.session_state:
+        st.session_state.df = load_df()
+    df = st.session_state.df
+    family = st.text_input('Family Name')
+    locker = st.text_input('Locker Number')
     if locker != '' and locker.isnumeric:
         locker = int(locker)
         if locker in df['Locker'].to_list():
-            show_combination(locker, df)
-            if st.button("That didn't work"):
-                next_combination(locker, df)
+            row = df[df['Locker'] == locker]
+            # st.write(row)
+            if (row['Assigned'] == family).all():
+                show_combination(locker, df)
+                if st.button("That didn't work"):
+                    next_combination(locker, df)
+                    
         else:
             st.write('Locker number is not assigned')
-        st.dataframe(df)
+        #st.dataframe(df)
 
